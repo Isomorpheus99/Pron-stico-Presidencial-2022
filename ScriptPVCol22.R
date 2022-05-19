7aa6247d657ba3f7efabd87a6e0da590f275af8b
Sergio Eduardo Calvo Mazuera <80175117+SergioECalvoM@users.noreply.github.com>
2022-05-19 15:52:00 -0500

    Update ScriptPVCol22.R

--- ScriptPVCol22.R
+++ ScriptPVCol22.R
@@ -7,11 +7,12 @@ rm(list=ls(all=TRUE))
 library(magrittr)
 library(readxl)
 library(tidyverse)
+library(plyr)
 library(kableExtra)
 library(lubridate)
 library(ggplot2)
 #Load data
-setwd("D:/Uni_PUJ/Semestre 4")
+setwd("C:/Users/sergi/OneDrive/Desktop/42/Work/Research/Election Forecasting/Colombia/Presidential")
 df<-read_xlsx("DataColPV2022.xlsx", sheet="Polls")
 dfweights<-read_xlsx("DataColPV2022.xlsx", sheet="Rating")
 clean_df<-df %>%
@@ -29,56 +30,76 @@ clean_df<-df %>%
 SimulationElect<-(S){
   dffinal=list()
   for(i in 1:S){
-  x<-rnrom(8)
-  x<-x/2
-  dfadjust<-clean_df%>%
-    mutate(Gustavo_Petro=Gustavo_Petro+x[1]*MoE, Fico_Gutierrez=Fico_Gutierrez+x[2]*MoE,
-           Sergio_Fajardo=Sergio_Fajardo+x[3]*MoE, Rodolfo_Hernandez=Rodolfo_Hernandez+x[4]*MoE,
-           Ingrid_Betancourt=Ingrid_Betancourt+x[5]*MoE, John_Rodriguez=John_Rodriguez+x[6]*MoE,
-           Enrique_Gomez=Enrique_Gomez+x[7]*MoE, En_Blanco=En_Blanco+x[8]*MoE)
-  random=runif(8)
-  for(i in 1:8){
-    random[[i]]=random[[i]]/sum(random)
+    x<-rnorm(8)
+    x<-x/2
+    dfadjust<-clean_df%>%
+      mutate(Gustavo_Petro=Gustavo_Petro+x[1]*MoE, Fico_Gutierrez=Fico_Gutierrez+x[2]*MoE,
+             Sergio_Fajardo=Sergio_Fajardo+x[3]*MoE, Rodolfo_Hernandez=Rodolfo_Hernandez+x[4]*MoE,
+             Ingrid_Betancourt=Ingrid_Betancourt+x[5]*MoE, John_Rodriguez=John_Rodriguez+x[6]*MoE,
+             Enrique_Gomez=Enrique_Gomez+x[7]*MoE, Luis_Perez=Luis_Perez, En_Blanco=En_Blanco+x[8]*MoE)
+    random=runif(8)
+    for(i in 1:8){
+      random[[i]]=random[[i]]/sum(random)
+    }
+    dfadjust<-dfadjust %>%
+      mutate(Gustavo_Petro=Gustavo_Petro+random[1]*voteava, 
+             Fico_Gutierrez=Fico_Gutierrez+random[2]*voteava, Sergio_Fajardo=Sergio_Fajardo+random[3]*voteava, 
+             Rodolfo_Hernandez=Rodolfo_Hernandez+random[4]*voteava, Ingrid_Betancourt=Ingrid_Betancourt+random[5]*voteava, 
+             Enrique_Gomez=Enrique_Gomez+random[6]*voteava,
+             John_Rodriguez=John_Rodriguez+random[7]*voteava, Luis_Perez=Luis_Perez, En_Blanco=En_Blanco+random[8]*voteava)%>%
+      left_join(dfweights)
+    dfadjust<-dfadjust %>%
+      mutate(Gustavo_Petro=if_else(Gustavo_Petro<0, 0, Gustavo_Petro), 
+             Fico_Gutierrez=if_else(Fico_Gutierrez<0, 0, Fico_Gutierrez),
+             Sergio_Fajardo=if_else(Sergio_Fajardo<0, 0, Sergio_Fajardo),
+             Rodolfo_Hernandez=if_else(Rodolfo_Hernandez<0, 0, Rodolfo_Hernandez),
+             Ingrid_Betancourt=if_else(Ingrid_Betancourt<0, 0, Ingrid_Betancourt),
+             John_Rodriguez=if_else(John_Rodriguez<0, 0, John_Rodriguez),
+             Enrique_Gomez=if_else(Enrique_Gomez<0, 0, Enrique_Gomez),
+             Luis_Perez=if_else(Luis_Perez<0, 0, Luis_Perez),
+             En_Blanco=if_else(En_Blanco<0, 0, En_Blanco))
+    currentdate=Sys.Date()
+    dfweighting<-dfadjust%>%
+      select(-(Rating))%>%
+      #filter(Pollster!="MassiveCaller")%>%
+      mutate(Age=as.numeric(currentdate-as.Date(Date)),
+             Decay=1/(MoE*Accuracy))
+    dfweighting<-dfweighting%>%
+      mutate(Time=((Decay*0.5)/mean(dfweighting$Decay))^(Age/30))
+    dfweighting<-dfweighting%>%
+      mutate(Weight=Time/sum(dfweighting$Time))
+    dffinal[[i]]<-dfweighting %>%
+      pivot_longer(cols=contains("_"), 
+                   names_to="nombre", values_to="Int_ajus_voto") %>%
+      mutate(Candidato=case_when(nombre=="Gustavo_Petro"~"Gustavo Petro", 
+                                 nombre=="Fico_Gutierrez"~"Federico Gutiérrez", 
+                                 nombre=="Sergio_Fajardo"~"Sergio Fajardo",
+                                 nombre=="Rodolfo_Hernandez"~"Rodolfo Hernández",
+                                 nombre=="John_Rodriguez"~"John M. Rodríguez",
+                                 nombre=="Luis_Perez"~"Luis Pérez",
+                                 nombre=="Enrique_Gomez"~"Enrique Gómez",
+                                 nombre=="Ingrid_Betancourt"~"Ingrid Betancourt",
+                                 nombre=="En_Blanco"~"Voto en Blanco")%>%
+               factor())%>%
+      group_by(Candidato)%>%
+      summarize(Predicción=signif(weighted.mean(Int_ajus_voto, Weight)*100, digits=4))%>%
+      arrange(desc(Predicción))
+    dffinal2<- dffinal%>%
+      select(-c(Candidato))
+    for(j in 1:ncol(dffinal[i])){
+      dffinal[i][j, 2]=dffinal2[i][j]/as.numeric(colSums(dffinal2))
+    }
   }
-  dfadjust<-dfadjust %>%
-    mutate(Gustavo_Petro=Gustavo_Petro+random[1]*voteava, 
-           Fico_Gutierrez=Fico_Gutierrez+random[2]*voteava, Sergio_Fajardo=Sergio_Fajardo+random[3]*voteava, 
-           Rodolfo_Hernandez=Rodolfo_Hernandez+random[4]*voteava, Ingrid_Betancourt=Ingrid_Betancourt+random[5]*voteava, 
-           Enrique_Gomez=Enrique_Gomez+random[6]*voteava,
-           John_Rodriguez=John_Rodriguez+random[7]*voteava, Luis_Perez=Luis_Perez, En_Blanco=En_Blanco+random[8]*voteava)%>%
-    left_join(dfweights)
-  
-  currentdate=Sys.Date()
-  dfweighting<-dfadjust%>%
-    select(-(Rating))%>%
-    #filter(Pollster!="MassiveCaller")%>%
-    mutate(Age=as.numeric(currentdate-as.Date(Date)),
-           Decay=1/(MoE*Accuracy))
-  dfweighting<-dfweighting%>%
-    mutate(Time=((Decay*0.5)/mean(dfweighting$Decay))^(Age/30))
-  dfweighting<-dfweighting%>%
-    mutate(Weight=Time/sum(dfweighting$Time))
-  dffinal[[i]]<-dfweighting %>%
-    pivot_longer(cols=contains("_"), 
-                 names_to="nombre", values_to="Int_ajus_voto") %>%
-    mutate(Candidato=case_when(nombre=="Gustavo_Petro"~"Gustavo Petro", 
-                               nombre=="Fico_Gutierrez"~"Federico Gutiérrez", 
-                               nombre=="Sergio_Fajardo"~"Sergio Fajardo",
-                               nombre=="Rodolfo_Hernandez"~"Rodolfo Hernández",
-                               nombre=="John_Rodriguez"~"John M. Rodríguez",
-                               nombre=="Luis_Perez"~"Luis Pérez",
-                               nombre=="Enrique_Gomez"~"Enrique Gómez",
-                               nombre=="Ingrid_Betancourt"~"Ingrid Betancourt",
-                               nombre=="En_Blanco"~"Voto en Blanco")%>%
-             factor())%>%
-    group_by(Candidato)%>%
-    summarize(signif(Predicción=weighted.mean(Int_ajus_voto, Weight)*100, digits=4))%>%
-    arrange(desc(Candidato))
-  dffinal2<- dffinal%>%
-    select(-c(Candidato))
-  for(j in ncol(dffinal[i])){
-    dffinal[i][j, 2]=dffinal2[i][j]/as.numeric(colSums(dffinal2))
-  }
-  }
-  Result=bind_rows(dffinal, .id = "Candidato")
+  Result<-join_all(dffinal, by="Candidato",type = "left", match = "all")
+  Dist<-Result
+  Result<-Result%>%
+    mutate(Promedio=rowMeans(Result[,2:ncol(Result)]))
+  Result<-Result%>%
+    select(c(Candidato, Promedio))
+  Return<-list("Dist"=Dist, "Result"=Result)
+  return(Return)
 }
+
+SimulationElect()
+install.packages("gmodels")
+apply(as.matrix(dffinal3), 1, function(x) ci(x))
